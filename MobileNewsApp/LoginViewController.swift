0023ae875e53e02369539c7c6d83a4cb7feefc74
Eitan-Yarmush <eyarmush212@gmail.com>
2017-02-18 16:55:59 -0600

    Added Facebook login to loginViewCOntroller

--- MobileNewsApp/LoginViewController.swift
+++ MobileNewsApp/LoginViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Parse
+import ParseFacebookUtilsV4
+
 
 class LoginViewController: UIViewController {
 
@@ -25,7 +29,83 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func login(_ sender: Any) {
+        
+        let email = emailField.text! as String
+        let password = passwordField.text! as String
+        
+        PFUser.logInWithUsername(inBackground: email, password: password, block: {(user: PFUser?, error: Error?) -> Void in
+            
+        })
+        
+    }
 
+    @IBAction func facebookLogin(_ sender: Any) {
+        
+        
+        PFFacebookUtils.logInInBackground(withReadPermissions: ["public_profile", "email"], block: {
+            (user: PFUser?, error: Error?) -> Void in
+            if let user = user {
+                if user.isNew {
+                    print("User signed up and logged in through Facebook!")
+                } else {
+                    print("User logged in through Facebook!")
+                }
+                
+                let requestParameters = ["fields": "id, email, first_name, last_name, picture.type(large)"]
+                let userDetails : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me", parameters: requestParameters)
+                
+                userDetails.start(completionHandler: { (connection, result, error) -> Void in
+                    if error != nil {
+                        print("Error getting Facebook Info", error ?? "")
+                        return
+                    } else if result != nil {
+                        //Get Current USer
+                        let myUser:PFUser = PFUser.current()!
+                        //Turn result into Dictionary
+                        let data:[String:AnyObject] = result as! [String : AnyObject]
+                        //Gather Data
+                        let userId:String? = data["id"] as? String
+                        let userFirstName:String? = data["first_name"] as? String
+                        let userLastName:String? = data["last_name"] as? String
+                        let userEmail:String? = data["email"] as? String
+                        
+                        
+                        //                      //Grab image URL, JSON is tricky in swift 3
+                        if let image = data["picture"] as? [String: Any] {
+                            if let imageData = image["data"] as? [String:Any] {
+                                let userImage:String? = imageData["url"] as! String?
+                                if (userImage != nil) { myUser.setValue(userImage, forKey: "fb_profile_picture") }
+                            }
+                        }
+                        
+                        //Set My User values to add data to parse user
+                        if(userId != nil) { myUser.setValue(userId, forKey: "fb_id") }
+                        if(userFirstName != nil) { myUser.setValue(userFirstName, forKey: "first_name") }
+                        if(userLastName != nil) { myUser.setValue(userLastName, forKey: "last_name") }
+                        if(userEmail != nil) { myUser.setValue(userEmail, forKey: "email") }
+                        //Save User data back to Parse
+                        myUser.saveInBackground(block: {(success: Bool, error: Error?) -> Void in
+                            if error != nil {
+                                print("Error saving User to DB", error ?? "")
+                                return
+                            }
+                            else if success {
+                                print("User successfully saved to DB")
+                            }
+                        })
+                        
+                        
+                    }
+                })
+                
+                
+            } else {
+                print("Uh oh. The user cancelled the Facebook login.")
+            }
+            
+        })
+    }
     /*
     // MARK: - Navigation
 
--- MobileNewsApp/RegistrationViewController.swift
+++ MobileNewsApp/RegistrationViewController.swift
@@ -44,73 +44,7 @@ class RegistrationViewController: UIViewController {
  
     
     @IBAction func SignIn(_ sender: UIButton) {
-        
-  
-        PFFacebookUtils.logInInBackground(withReadPermissions: ["public_profile", "email"], block: {
-            (user: PFUser?, error: Error?) -> Void in
-            if let user = user {
-                if user.isNew {
-                    print("User signed up and logged in through Facebook!")
-                } else {
-                    print("User logged in through Facebook!")
-                }
-                
-                let requestParameters = ["fields": "id, email, first_name, last_name, picture.type(large)"]
-                let userDetails : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "/me", parameters: requestParameters)
-                
-                userDetails.start(completionHandler: { (connection, result, error) -> Void in
-                    if error != nil {
-                        print("Error getting Facebook Info", error ?? "")
-                        return
-                    } else if result != nil {
-                        //Get Current USer
-                        let myUser:PFUser = PFUser.current()!
-                        //Turn result into Dictionary
-                        let data:[String:AnyObject] = result as! [String : AnyObject]
-                        //Gather Data
-                        let userId:String? = data["id"] as? String
-                        let userFirstName:String? = data["first_name"] as? String
-                        let userLastName:String? = data["last_name"] as? String
-                        let userEmail:String? = data["email"] as? String
-                        
-                        self.userId = userId
-                        
-                        self.performSegue(withIdentifier: "registrationSegue", sender: self)
-                        
-//                      //Grab image URL, JSON is tricky in swift 3
-                        if let image = data["picture"] as? [String: Any] {
-                            if let imageData = image["data"] as? [String:Any] {
-                                let userImage:String? = imageData["url"] as! String?
-                                if (userImage != nil) { myUser.setValue(userImage, forKey: "fb_profile_picture") }
-                            }
-                        }
-                        
-                        //Set My User values to add data to parse user
-                        if(userId != nil) { myUser.setValue(userId, forKey: "fb_id") }
-                        if(userFirstName != nil) { myUser.setValue(userFirstName, forKey: "first_name") }
-                        if(userLastName != nil) { myUser.setValue(userLastName, forKey: "last_name") }
-                        if(userEmail != nil) { myUser.setValue(userEmail, forKey: "email") }
-                        //Save User data back to Parse
-                        myUser.saveInBackground(block: {(success: Bool, error: Error?) -> Void in
-                            if error != nil {
-                                print("Error saving User to DB", error ?? "")
-                                return
-                            }
-                            else if success {
-                                print("User successfully saved to DB")
-                            }
-                        })
-    
-                            
-                    }
-                })
-                
-                
-            } else {
-                print("Uh oh. The user cancelled the Facebook login.")
-            }
- 
-        })
+
 
     }
     
