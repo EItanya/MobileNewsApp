d44a83812e2596688e0da788d1d8902921bd2dad
Jonathan Nguyen <jtlnguyen@utexas.edu>
2017-02-24 22:52:21 -0600

    Add initial tableview for Home tab

---
+++ MobileNewsApp/JoinStoryTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  JoinStoryTableViewCell.swift
+//  MobileNewsApp
+//
+//  Created by Nelia Perez on 2/24/17.
+//  Copyright © 2017 cs378. All rights reserved.
+//
+
+import UIKit
+
+class JoinStoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var promptLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var wordCountLabel: UILabel!
+    @IBOutlet var peopleAheadLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
