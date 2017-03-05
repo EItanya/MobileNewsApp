78f50ccc7a40f7a8aabe64273797730e60a61495
Jonathan Nguyen <jtlnguyen@utexas.edu>
2017-03-05 00:43:59 -0600

    Edit Join Table Viewto list only valid stories

--- MobileNewsApp/HomeViewController.swift
+++ MobileNewsApp/HomeViewController.swift
@@ -36,7 +36,7 @@ class HomeViewController: UIViewController {
     var stories = [Story]()
     var filteredStories = [Story]()
     
-    var storiesStoryObject = [Story]()
+    //var storiesStoryObject = [Story]()
     
     
     //table view of stories
@@ -52,12 +52,13 @@ class HomeViewController: UIViewController {
 
         storyTableView.rowHeight = UITableViewAutomaticDimension
         storyTableView.estimatedRowHeight = 140
-        
+
         Story.getAllStories() {
             (stories: [Story]?, Error) in
-            self.unfinishedStories = stories!.filter { $0.completed == false }
+            let validStories = stories!.filter { $0.users.contains(PFUser.current()!.objectId!) == false }
+            self.unfinishedStories = validStories.filter { $0.completed == false }
             self.unfinishedFilteredStories = self.unfinishedStories
-            self.completedStories = stories!.filter { $0.completed == true }
+            self.completedStories = validStories.filter { $0.completed == true }
             self.completedFilteredStories = self.completedStories
             self.stories = self.completedStories
             self.filteredStories = self.completedStories
