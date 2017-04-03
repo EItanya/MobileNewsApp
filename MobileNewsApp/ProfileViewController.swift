7ac99ae716c4bd26d676aa812ce543ca15bc9e9f
mantu_nguyen <nguyen.mantuphan@gmail.com>
2017-04-02 21:48:30 -0500

    some minor fixes

--- MobileNewsApp/ProfileViewController.swift
+++ MobileNewsApp/ProfileViewController.swift
@@ -44,11 +44,9 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
     
     //completedStories
     var completedStories = [Story]()
-    var completedFilteredStories = [Story]()
     
     //stories to populate views
     var stories = [Story]()
-    var filteredStories = [Story]()
     
     var storiesStoryObject = [Story]()
     var isMyTurn = false
@@ -62,23 +60,21 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //scrollView.delegate = self
-        ///
         profileTableView.delegate = self
         profileTableView.dataSource = self
         profileTableView.rowHeight = UITableViewAutomaticDimension
         profileTableView.estimatedRowHeight = 140
-        
+      
+        Story.getUserStoriesArray { (stories, Error) in
+            self.unfinishedStories = stories!.filter { $0.completed == false }
+            self.completedStories = stories!.filter { $0.completed == true }
+            self.stories = self.unfinishedStories + self.completedStories
+            self.profileTableView.reloadData()
+        }
 
     }
     
     override func viewDidAppear(_ animated: Bool) {
-    
-        // Header - Image
-//        headerImageView = UIImageView(frame: header.bounds)
-//        headerImageView?.image = UIImage(named: "header_bg")
-//        headerImageView?.contentMode = UIViewContentMode.scaleAspectFill
-        //header.insertSubview(headerImageView, belowSubview: headerLabel)
 
         let user = PFUser.current()
         let id = user?.objectId
@@ -86,18 +82,6 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
 
         print("my id is \(id)")
         print("my userId is \(userId)")
-        
-
-        
-//        Story.getUserStories(userId: id!) {
-//            (stories:[Story]?, Error) in
-//            //print("how about here id is \(id!)")
-//
-//            self.unfinishedStories = stories!.filter { $0.completed == false }
-//            self.completedStories = stories!.filter { $0.completed == true }
-//            self.stories = self.unfinishedStories
-//            self.profileTableView.reloadData()
-//        }
         
         Story.getUserStoriesArray { (stories, Error) in
             self.unfinishedStories = stories!.filter { $0.completed == false }
@@ -111,20 +95,7 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         let invites = [Invite]()
         let numInvites = invites.count
         print("This is the number of invites i have \(numInvites)")
-        //self.notificationCount.text = String(numInvites)
-        //self.notificationImage.setImage(#imageLiteral(resourceName: "Invite"), for: UIControlState.normal)
-        //self.notificationImage.setTitle("", for: UIControlState.normal)
-        
-        //let img = userImageDict[user]
-        
-        //let image = (user?.object(forKey: "fb_profile_picture") as! UIImage)
-        //print("This is my link to the picture: \(image)")
-        //self.profileImage.image = image
-        
-        //let imageString = (user?.object(forKey: "fb_profile_picture"))
 
-        
-       // let url = URL(string: image.imageString)
         let session = URLSession(configuration: .default)
         let url = user!["fb_profile_picture"] as? String
         let pictureUrl = URL(string: url!)
@@ -166,31 +137,11 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         button.setBackgroundImage(image, for: .normal)
         
         let rightButton = BBBadgeBarButtonItem(customUIButton: button)
-        rightButton?.badgeValue = "1"
+        rightButton?.badgeValue = "\(numInvites)"
         rightButton?.badgeOriginX = 20
         rightButton?.badgeOriginY = -5
         navigationItem.rightBarButtonItem = rightButton
         
-//        //let navRightButton = BBBadgeBarButtonItem(customUIButton: notification)
-//        let button = UIButton(type: .custom)
-//        //button.setAttributedTitle(notificationImage.attributedString(), for: .Normal)
-//        let notification = BBBadgeBarButtonItem(customUIButton: button)
-//        //notificationImage.frame = CGRect(x:0, y:0, width:44, height:44)
-//        //notification?.badgeValue = "\(numInvites)"
-//        notification?.badgeValue = "1"
-//
-//        //notification?.shouldHideBadgeAtZero = false
-//        //notification?.shouldAnimateBadge = true
-//        notification?.badgeOriginX = 5
-//        notification?.badgeOriginY = 5
-//        self.navigationItem.rightBarButtonItem = notification
-    
-        
-
-//        let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-//        imageView.image = UIImage(data: data!)
-        
-        //header.clipsToBounds = true
     }
     
 
@@ -234,71 +185,6 @@ class ProfileViewController: UIViewController, UIScrollViewDelegate {
         }
         profileTableView.reloadData()
     }
-    
-
-    
-    
-    
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        
-//        let offset = scrollView.contentOffset.y
-//        var avatarTransform = CATransform3DIdentity
-//        var headerTransform = CATransform3DIdentity
-//        
-//        // PULL DOWN -----------------
-//        
-//        if offset < 0 {
-//            
-//            let headerScaleFactor:CGFloat = -(offset) / header.bounds.height
-//            let headerSizevariation = ((header.bounds.height * (1.0 + headerScaleFactor)) - header.bounds.height)/2.0
-//            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
-//            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
-//            
-//            header.layer.transform = headerTransform
-//        }
-//            
-//            // SCROLL UP/DOWN ------------
-//            
-//        else {
-//            
-//            // Header -----------
-//            
-//            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
-//            
-//            //  ------------ Label
-//            
-//            let labelTransform = CATransform3DMakeTranslation(0, max(-distance_W_LabelHeader, offset_B_LabelHeader - offset), 0)
-//            headerLabel.layer.transform = labelTransform
-//            
-//            //  ------------ Blur
-//            
-//            headerBlurImageView?.alpha = min (1.0, (offset - offset_B_LabelHeader)/distance_W_LabelHeader)
-//            
-//            // Avatar -----------
-//            
-//            let avatarScaleFactor = (min(offset_HeaderStop, offset)) / avatarImage.bounds.height / 1.4 // Slow down the animation
-//            let avatarSizeVariation = ((avatarImage.bounds.height * (1.0 + avatarScaleFactor)) - avatarImage.bounds.height) / 2.0
-//            avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
-//            avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
-//            
-//            if offset <= offset_HeaderStop {
-//                
-//                if avatarImage.layer.zPosition < header.layer.zPosition{
-//                    header.layer.zPosition = 0
-//                }
-//                
-//            }else {
-//                if avatarImage.layer.zPosition >= header.layer.zPosition{
-//                    header.layer.zPosition = 2
-//                }
-//            }
-//        }
-//        
-//        // Apply Transformations
-//        
-//        header.layer.transform = headerTransform
-//        avatarImage.layer.transform = avatarTransform
-//    }
     
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -346,23 +232,3 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
 }
-
-//extension ProfileViewController {
-//    func setupNotification() {
-//        let button = UIButton(type: .custom)
-//        let notificationImage = "Invite"
-//        let image = UIImage(named: notificationImage)
-//        let imageView = UIImageView(image: image!)
-//        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
-//        view.addSubview(imageView)
-//
-//        //button.setAttributedTitle(imageView.attributedString(), for: .Normal)
-//        button.addTarget(self, action: Selector(("buttonClicked")), for: UIControlEvents.touchUpInside)
-//        let notification = BBBadgeBarButtonItem(customUIButton: button)
-//        button.frame = CGRect(x:0, y:0, width:44, height:44)
-//        notification?.badgeValue = num.
-//        
-//        self.navigationItem.rightBarButtonItem = notification
-//        
-//    }
-//}
